4f6760861b3d687ecc233949b1c946f9d5a99712
Matt Siskey <34102014+Matt-Siskey@users.noreply.github.com>
2021-12-09 17:05:21 -0800

    Code defining functions used to generate seed lists, and write AMAK stock assessment model files (file writer functions not shared publically here).

---
+++ Functions.R
@@ -0,0 +1,25 @@
+###############################################
+### AMAK Functions
+### February 2021
+###############################################
+
+#Dusky Rockfish AMAK Data File Generator
+source("C:/Users/matthew.siskey/Desktop/2020_UW-AFSC Post-Doc/Data & Code/otosamp/Functions/writeDat_dusky.R")
+
+#Dusky Rockfish AMAK Control File Generator
+source("C:/Users/matthew.siskey/Desktop/2020_UW-AFSC Post-Doc/Data & Code/otosamp/Functions/writeCtl_dusky.R")
+
+#Walley Pollock AMAK Data File Generator
+source("C:/Users/matthew.siskey/Desktop/2020_UW-AFSC Post-Doc/Data & Code/otosamp/Functions/writeDat_pollock.R")
+
+#Pacific Ocean Perch AMAK Data File Generator
+source("C:/Users/matthew.siskey/Desktop/2020_UW-AFSC Post-Doc/Data & Code/otosamp/Functions/writeDat_POP.R")
+
+#Pacific Ocean Perch AMAK Control File Generator
+source("C:/Users/matthew.siskey/Desktop/2020_UW-AFSC Post-Doc/Data & Code/otosamp/Functions/writeCtl_POP.R")
+
+#Walley Pollock AMAK Data File Generator for base model (no change in proportions-at-age)
+source("C:/Users/matthew.siskey/Desktop/2020_UW-AFSC Post-Doc/Data & Code/otosamp/Functions/writeDat_pollock_base.R")
+
+#Seeds
+#source("C:/Users/matthew.siskey/Desktop/2020_UW-AFSC Post-Doc/Data & Code/otosamp/Functions/Seeds.R")
