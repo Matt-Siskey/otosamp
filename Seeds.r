6140cb8acbdd6a5b931d4d2387a2a491a67f89a1
Matt Siskey <34102014+Matt-Siskey@users.noreply.github.com>
2022-03-10 10:55:42 -0800

    Function for setting the seed
    Function for setting the seed

---
+++ Seeds.r
@@ -0,0 +1,23 @@
+###############################################
+### Bootstrap Estimator
+### Seeds
+### Matt Siskey
+### August 2021
+###############################################
+
+Seed <- function(n){
+  seed <- round(runif(n,1,10000000),0)
+  return(seed)
+}
+
+i=10000
+seeds <- matrix(0,i,2)
+
+seeds[,1]  <- Seed(i)
+seeds[,2]  <- Seed(i)
+
+
+#seeds[1,2] = 679877 #random number
+colnames(seeds)=c("bootest.seed1","bootest.seed2")
+seed.list=list(seeds)
+save(seed.list,file=paste0(save_dir,"/seed.list"))
