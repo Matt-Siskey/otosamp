4cc2d3723c50f22966f120f1a28562304dd2e449
Matt Siskey <34102014+Matt-Siskey@users.noreply.github.com>
2022-03-10 10:55:22 -0800

    Functions for writing model files & seeding
    Functions for writing model files & seeding

---
+++ Functions.R
@@ -0,0 +1,22 @@
+###############################################
+### AMAK Functions
+### February 2021
+###############################################
+
+#Dusky Rockfish AMAK Data File Generator
+source(paste0(fun_dir,"/Functions/writeDat_dusky.R"))
+
+#Dusky Rockfish AMAK Control File Generator
+source(paste0(fun_dir,"/Functions/writeCtl_dusky.R"))
+
+#Walley Pollock AMAK Data File Generator
+source(paste0(fun_dir,"/Functions/writeDat_pollock.R"))
+
+#Pacific Ocean Perch AMAK Data File Generator
+source(paste0(fun_dir,"Functions/writeDat_POP.R"))
+
+#Pacific Ocean Perch AMAK Control File Generator
+source(paste0(fun_dir,"/Functions/writeCtl_POP.R"))
+
+#Seeds
+source(paste0(fun_dir,"/Functions/Seeds.R"))
